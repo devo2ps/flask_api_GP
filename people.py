eea9f05ae368eff33a4cbf921a35d61bcb46008b
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-03-20 00:33:44 -0500

    dded the rest (GET, POST, PUT, DELETE) of the API endpoints and relevant functions in people.py

--- people.py
+++ people.py
@@ -2,6 +2,8 @@
 
 
 from datetime import datetime
+from flask import abort
+from flask import abort, make_response
 
 
 def get_timestamp():
@@ -47,3 +49,71 @@ PEOPLE = {
 def read_all():
 
     return list(PEOPLE.values())
+    
+    
+def create(person): #creates person obj, related to POST operation in yml file
+
+    lname = person.get("lname")
+
+    fname = person.get("fname", "")
+
+
+    if lname and lname not in PEOPLE:
+
+        PEOPLE[lname] = {
+
+            "lname": lname,
+
+            "fname": fname,
+
+            "timestamp": get_timestamp(),
+
+        }
+
+        return PEOPLE[lname], 201
+
+    else:
+
+        abort(
+
+            406,
+
+            f"Person with last name {lname} already exists",
+
+        )
+        
+        
+def read_one(lname): #returns data if last name found by Flask app, related to GET operation in yml file
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+        
+        
+def update(lname, person): #updates person obj, related to PUT operation in yml file
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+
+def delete(lname): #removes person obj, related to DELETE operation in yml file
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+
