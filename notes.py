fe809599484d6d797b405c74aa77c339818a4f48
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-03-30 23:57:44 -0500

    added create(note) to notes.py and the note api endpoint (above notes/note_id) in the yml file. This finishes the basic api/flask app for now, but it still needs a front end!

--- notes.py
+++ notes.py
@@ -3,7 +3,9 @@
 from flask import abort, make_response
 
 from config import db
-from models import Note, note_schema
+from models import Note, note_schema, Person
+
+
 
 def read_one(note_id):
     note = Note.query.get(note_id)
@@ -13,4 +15,20 @@ def read_one(note_id):
     else:
         abort(
             404, f"Note with ID {note_id} not found"
+        )
+
+
+def create(note):
+    person_id = note.get("person_id")
+    person = Person.query.get(person_id)
+
+    if person:
+        new_note = note_schema.load(note, session=db.session)
+        person.notes.append(new_note)
+        db.session.commit()
+        return note_schema.dump(new_note), 201
+    else:
+        abort(
+            404,
+            f"Person not found for ID: {person_id}"
         )
